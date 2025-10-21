c1a437be98eb6e730854af1f5f840f4d1eeed562
DilshanGDT <tharakadilshan506@gmail.com>
2025-10-21 10:18:06 +0530

    Initial commit: Spring Boot Auth Service

---
+++ src/main/java/com/eyepax/authservice/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eyepax.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
